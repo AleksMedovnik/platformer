623ef1fefa0e7634fb3e9caf06494ccd43f7b799
Aleksandr <medovnikaleksander@yandex.ua>
2022-01-13 15:33:56 +0300

    Delete message

--- js/main.js
+++ js/main.js
@@ -4,15 +4,6 @@ import MainScene from "./scene/mainScene.js";
 
 const main = document.getElementById('main');
 
-const resize = () => {
-    if(document.documentElement.clientWidth < 1000){
-        alert(`Просматриваемое окно слишком мало! Пожалуйста, увеличьте область просмотра!`)
-    }
-}
-resize();
-
-window.onresize = resize;
-
 const config = {
     type: Phaser.AUTO,
     width: 1000,
